60cb04ae7c0011681198bef34e25a6a9389c6116
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2024-05-29 03:30:08 -0700

    adding fixes

--- backend/app.js
+++ backend/app.js
@@ -1,54 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const session = require("express-session");
 const sequelize = require("./db");
 const Employee = require("./models/Employee");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var apiRouter = require("./routes/api");
 var app = express();
 const cors = require("cors");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(cors());
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter)
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 async function setup() {
-  const employeeTest = await Employee.create({ firstName: "Justin", lastName: "Lawrence", employeeID: 95, position: "Manager", wage: 24.00 });
-  console.log("employee test instance created...");}
-
-  sequelize.sync({ force: true }).then(() => {
-    console.log("Sequelize Sync Completed...");
-    setup().then(() => console.log("User setup complete"));
+  const employeeTest = await Employee.create({
+    firstName: "Justin",
+    lastName: "Lawrence",
+    employeeID: 95,
+    position: "Manager",
+    wage: 24.0,
   });
+  console.log("employee test instance created...");
+}
+
+sequelize.sync({ force: true }).then(() => {
+  console.log("Sequelize Sync Completed...");
+  setup().then(() => console.log("User setup complete"));
+});
 module.exports = app;
--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -1,50 +1,55 @@
-const sequelize = require('../db')
-const { Model, DataTypes } = require('sequelize')
+const sequelize = require("../db");
+const { Model, DataTypes } = require("sequelize");
 
 class Employee extends Model {
-
-    static async findEmployee(employeeID){
-        try {
-            const employee = await Employee.findByPk(employeeID)
-            if(employee && employee.employeeID === employeeID){
-                return employee
-            }else{
-                return null
-            }
-        } catch (error) {
-            console.log(error)
-            return null
-        }
+  static async findEmployee(employeeID) {
+    try {
+      const employee = await Employee.findByPk(employeeID);
+      if (employee && employee.employeeID === employeeID) {
+        return employee;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
     }
+  }
 
+  static async getEmployees() {
+    const employees = await Employee.findAll();
+    return employees;
+  }
 }
 
-Employee.init({
-  firstName: {
-    type: DataTypes.STRING,
-    
-    allowNull: false
+Employee.init(
+  {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    employeeID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    position: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    wage: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
   },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  employeeID: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    allowNull: false
-  },
-  position: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  wage: {
-    type: DataTypes.DOUBLE,
-    allowNull: false
-  },
-}, {
-  sequelize, 
-  modelName: 'Employee'
-});
+  {
+    sequelize,
+    modelName: "Employee",
+  }
+);
 
-module.exports = Employee
+module.exports = Employee;
